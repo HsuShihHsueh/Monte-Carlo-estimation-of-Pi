633cab32f1da4771f92a8d9229071bc25fef2cc7
HsuShihHsueh <77485425+HsuShihHsueh@users.noreply.github.com>
2022-09-03 21:44:46 +0800

    Update monte_carlo.py

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,7 +3,7 @@ import numpy as np
 n = 1_000_000
 tmp = np.random.rand(n)
 tmp = np.sqrt(1-tmp**2)
-tmp = 4*tmp.mean()
+tmp = 4 * tmp.mean()
 print('The estimate result of average value method: ',tmp)
 
 #method2: area method
@@ -11,5 +11,5 @@ import numpy as np
 n = 1_000_000
 tmp = np.random.rand(n,2)
 tmp = np.square(tmp).sum(axis=1)
-tmp = (tmp<1).sum() / n * 4
+tmp = 4 * (tmp<1).mean()
 print('The estimate result of area method: ',tmp)
